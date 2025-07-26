94cea5779bc813355ff6c9c7691b88f482c5f862
JorgeIP10 <jorgep.ipanaquep@gmail.com>
2025-07-25 23:03:50 -0500

    chore: change url variable

--- app/constants.py
+++ app/constants.py
@@ -3,6 +3,6 @@ import os
 
 load_dotenv()
 
-PRODUCER_HTTP_URL = f'http://{os.getenv("PRODUCER_URL")}'
-PRODUCER_WS_URL = f'ws://{os.getenv("PRODUCER_URL")}'
+PRODUCER_HTTP_URL = os.getenv("PRODUCER_HTTP_URL")
+PRODUCER_WS_URL = os.getenv("PRODUCER_WS_URL")
 
